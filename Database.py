b15ed2485861e14a6659ba265f816c3067e9905c
UsmanArshad-Cloud <muhammad.usman@ocloudsolutions.net>
2023-08-23 12:24:54 +0500

    Integration with POSTGRES

--- Database.py
+++ Database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./TODO_TASK.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./TODO_TASK.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:997480@localhost:5434/TodoApplicationDataBase"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, bind=engine)
 
